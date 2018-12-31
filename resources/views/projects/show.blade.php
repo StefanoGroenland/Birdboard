2be59c8db27fe2ac4d37d4337e0524fb5d524596
Stefano Groenland <stefano@pos4.nl>
2018-12-31 08:35:04 +0100

    Model Tests

---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ $project->title }}</title>
+</head>
+<body>
+    <h1>{{ $project->title }}</h1>
+    <div>{{ $project->description }}</div>
+</body>
+</html>
